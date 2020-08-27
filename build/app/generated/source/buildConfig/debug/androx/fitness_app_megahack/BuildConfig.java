b987df30b3b20dd812edab62f9689f5e2ca216a7
shahi1 <you@example.com>
2020-08-27 15:11:46 +0545

    basic widgets added

---
+++ build/app/generated/source/buildConfig/debug/androx/fitness_app_megahack/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package androx.fitness_app_megahack;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "androx.fitness_app_megahack";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
