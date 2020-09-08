402aaf1cea810fabed451d6a1681713785fa6970
shahi1 <you@example.com>
2020-09-08 17:29:08 +0545

    home screen with pedometer added

---
+++ lib/home/home.dart
@@ -0,0 +1,90 @@
+import 'package:fitness_app_megahack/home/screens/Pedometer_1.dart';
+import 'package:fitness_app_megahack/home/screens/second.dart';
+import 'package:fitness_app_megahack/home/screens/third.dart';
+import 'package:fitness_app_megahack/home/screens/fourth.dart';
+import 'package:fitness_app_megahack/home/screens/fifth.dart';
+import 'package:flutter/material.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class BottomNavBar extends StatefulWidget {
+  static const String id = 'BottomNavBar';
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int pageIndex = 0;
+  GlobalKey _bottomNavigationKey = GlobalKey();
+
+  final Exercise _sites = new Exercise();
+  final Stepcount _dashboard = new Stepcount();
+  final Second _clients = new Second();
+  final Fourth _flutterpages = new Fourth();
+  final Fifth _pageother = new Fifth();
+
+  Widget _showPage = new Stepcount();
+
+  Widget _pageChooser(int page) {
+    switch (page) {
+      case 0:
+        return _dashboard;
+        break;
+      case 1:
+        return _clients;
+        break;
+      case 2:
+        return _sites;
+        break;
+      case 3:
+        return _flutterpages;
+        break;
+      case 4:
+        return _pageother;
+        break;
+      default:
+        return new Center(
+          child: new Center(
+            child: new Text(
+              '404 Page Not Found',
+              style: new TextStyle(fontSize: 30),
+            ),
+          ),
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: CurvedNavigationBar(
+        key: _bottomNavigationKey,
+        index: pageIndex,
+        height: 50.0,
+        items: <Widget>[
+          Icon(FontAwesomeIcons.shoePrints, size: 23),
+          Icon(FontAwesomeIcons.utensils, size: 30),
+          Icon(FontAwesomeIcons.dumbbell, size: 30),
+          Icon(FontAwesomeIcons.fileMedicalAlt, size: 30),
+          Icon(FontAwesomeIcons.map, size: 30),
+        ],
+        color: Colors.blueAccent,
+        buttonBackgroundColor: Colors.blueAccent,
+        backgroundColor: Colors.white,
+        animationCurve: Curves.easeInOut,
+        animationDuration: Duration(milliseconds: 600),
+        onTap: (int tappedIndex) {
+          setState(() {
+            _showPage = _pageChooser(tappedIndex);
+          });
+        },
+      ),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: _showPage,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home/screens/Pedometer_1.dart
@@ -0,0 +1,378 @@
+import 'dart:math';
+import 'package:fitness_app_megahack/auth.dart';
+import 'package:fitness_app_megahack/bmi/screens/input_page.dart';
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:pedometer/pedometer.dart';
+import 'package:provider/provider.dart';
+
+class Stepcount extends StatefulWidget {
+  @override
+  _StepcountState createState() => _StepcountState();
+}
+
+class _StepcountState extends State<Stepcount> {
+  String muestrePasos = "";
+  String _km = "Unknown";
+  String _calories = "Unknown";
+
+  String _stepCountValue = 'Unknown';
+  StreamSubscription<int> _subscription;
+
+  double _numerox; //numero pasos
+  double _convert;
+  double _kmx;
+  double burnedx;
+  double _porciento;
+  // double percent=0.1;
+
+  @override
+  void initState() {
+    super.initState();
+    //initPlatformState();
+    setUpPedometer();
+  }
+
+  //inicia codigo pedometer
+  void setUpPedometer() {
+    Pedometer pedometer = new Pedometer();
+    _subscription = pedometer.stepCountStream.listen(_onData,
+        onError: _onError, onDone: _onDone, cancelOnError: true);
+  }
+
+  void _onData(int stepCountValue) async {
+    print(stepCountValue); //impresion numero pasos por consola
+    setState(() {
+      _stepCountValue = "$stepCountValue";
+      print(_stepCountValue);
+    });
+
+    var dist = stepCountValue; //pasamos el entero a una variable llamada dist
+    double y = (dist + .0); //lo convertimos a double una forma de varias
+
+    setState(() {
+      _numerox =
+          y; //lo pasamos a un estado para ser capturado ya convertido a double
+    });
+
+    var long3 = (_numerox);
+    long3 = num.parse(y.toStringAsFixed(2));
+    var long4 = (long3 / 10000);
+
+    int decimals = 1;
+    int fac = pow(10, decimals);
+    double d = long4;
+    d = (d * fac).round() / fac;
+    print("d: $d");
+
+    getDistanceRun(_numerox);
+
+    setState(() {
+      _convert = d;
+      print(_convert);
+    });
+  }
+
+  void reset() {
+    setState(() {
+      int stepCountValue = 0;
+      stepCountValue = 0;
+      _stepCountValue = "$stepCountValue";
+    });
+  }
+
+  void _onDone() {}
+
+  void _onError(error) {
+    print("Flutter Pedometer Error: $error");
+  }
+
+  //function to determine the distance run in kilometers using number of steps
+  void getDistanceRun(double _numerox) {
+    var distance = ((_numerox * 78) / 100000);
+    distance = num.parse(distance.toStringAsFixed(2)); //dos decimales
+    var distancekmx = distance * 34;
+    distancekmx = num.parse(distancekmx.toStringAsFixed(2));
+    //print(distance.runtimeType);
+    setState(() {
+      _km = "$distance";
+      //print(_km);
+    });
+    setState(() {
+      _kmx = num.parse(distancekmx.toStringAsFixed(2));
+    });
+  }
+
+  //function to determine the calories burned in kilometers using number of steps
+  void getBurnedRun() {
+    setState(() {
+      var calories = _kmx; //dos decimales
+      _calories = "$calories";
+      //print(_calories);
+    });
+  }
+
+  //fin codigo pedometer
+
+  @override
+  Widget build(BuildContext context) {
+    // print(_stepCountValue);
+    // print(int.parse(_stepCountValue).runtimeType);
+    getBurnedRun();
+    return new MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: new Scaffold(
+        appBar: new AppBar(
+            title: const Text('Step Counter'),
+            backgroundColor: Colors.blueAccent,
+            actions: [
+              Container(
+                child: RaisedButton(
+                  color: Colors.blueAccent,
+                  onPressed: () {
+                    Provider.of<Auth>(context, listen: false).googlelogout();
+
+                    Navigator.pop(context);
+                  },
+                  child: Text(
+                    "Logout",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ),
+              )
+            ]),
+        body: new ListView(
+          padding: EdgeInsets.all(5.0),
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircularPercentIndicator(
+                  radius: 230.0,
+                  lineWidth: 20.0,
+                  percent: _stepCountValue == 'Unknown'
+                      ? 0
+                      : double.parse(_stepCountValue) / 10000000000,
+                  // percent: 0.5,
+                  animation: true,
+                  center: Container(
+                    child: Center(
+                      child: new Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          SizedBox(
+                            width: 50,
+                          ),
+                          Container(
+                            height: 50,
+                            width: 50,
+                            padding: EdgeInsets.only(left: 20.0),
+                            child: Icon(
+                              FontAwesomeIcons.walking,
+                              size: 30.0,
+                              color: Colors.blueAccent,
+                            ),
+                          ),
+                          Container(
+                            //color: Colors.orange,
+                            child: Text(
+                              _stepCountValue == 'Unknown'
+                                  ? '0'
+                                  : '$_stepCountValue',
+                              // 'Walk',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20.0,
+                                  color: Colors.blueAccent),
+                            ),
+                            // height: 50.0,
+                            // width: 50.0,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  // percent: _convert,
+                  circularStrokeCap: CircularStrokeCap.round,
+                  progressColor: Colors.blueAccent,
+                ),
+                // RaisedButton(
+                //   onPressed: () {
+                //     Provider.of<Auth>(context, listen: false).googlelogout();
+
+                //     Navigator.pop(context);
+                //   },
+                //   child: Text("Logout"),
+                // )
+              ],
+            ),
+            Center(
+              child: Text(
+                'Daily Goal =10000',
+                style: TextStyle(fontSize: 30),
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        '         DISTANCE',
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '         $_km',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: 'km',
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Expanded(
+                  flex: 3,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'CALORIES',
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                      RichText(
+                        text: TextSpan(
+                          children: [
+                            TextSpan(
+                              text: '$_kmx',
+                              style: TextStyle(
+                                fontSize: 20,
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            TextSpan(
+                              text: ' cal',
+                              style: TextStyle(
+                                color: Colors.black87,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    InkWell(
+                        child: Container(
+                          height: 140.0,
+                          width: 180.0,
+                          decoration: new BoxDecoration(
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(50.0)),
+                            image: DecorationImage(
+                              image: new AssetImage('assets/Bmi/bmi.jpg'),
+                              fit: BoxFit.contain,
+                            ),
+                            shape: BoxShape.rectangle,
+                            border: Border.all(
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => InputPage()),
+                          );
+                        }),
+                    Text(
+                      "BMI Calculator",
+                      style: TextStyle(
+                        color: Colors.black,
+                        //    fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    )
+                  ],
+                ),
+                Column(
+                  children: <Widget>[
+                    InkWell(
+                      child: Container(
+                        height: 140.0,
+                        width: 180.0,
+                        decoration: new BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                          image: DecorationImage(
+                            image: new AssetImage('assets/Bmi/heart.jpg'),
+                            fit: BoxFit.contain,
+                          ),
+                          shape: BoxShape.rectangle,
+                          border: Border.all(
+                            width: 1,
+                          ),
+                        ),
+                      ),
+                    ),
+                    Text(
+                      "Heart Rate",
+                      style: TextStyle(
+                        color: Colors.black,
+                        //         fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
